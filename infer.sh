ddb94053367a981938257d344c2f81a6df2c0b1c
费政聪 <3300949577@qq.com>
2025-01-01 20:58:38 +0800

    Add files via upload

---
+++ infer.sh
@@ -0,0 +1,3 @@
+python infer.py \
+    --prompt "Two men in half bodies, are seated in a dimly lit room, possibly an office or meeting room, with a formal atmosphere." \
+    --img_file_path 'asserts/0.jpg' 'asserts/1.jpg'
